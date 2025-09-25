e26b95cc3f1969fc7a8909708f64d4f73a4ae4a0
gturrisi <giulio.turrisi@iit.it>
2025-09-25 16:35:26 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,133 @@
+# Overview
+
+This repository is about trash collection using RL on quadruped robots, with sim-to-sim and sim-to-real scripts. 
+
+Features:
+- [Cuncurrent State Estimator](https://arxiv.org/pdf/2202.05481)
+- [Rapid Motor Adaptation](https://arxiv.org/pdf/2107.04034)
+- [Morphologycal Symmetries](https://arxiv.org/pdf/2403.17320) 
+- [Adversarial Motion Priors](https://arxiv.org/pdf/2104.02180)
+- Sim-to-Sim in [Mujoco](https://github.com/google-deepmind/mujoco)
+- Sim-to-Real in ROS1 and ROS2
+
+A list of robots available and envs are described below:
+
+| Robot Model         | Environment Name (ID)                                      |
+|---------------------|------------------------------------------------------------|
+| [Aliengo](https://github.com/iit-DLSLab/gym-quadruped/tree/master/gym_quadruped/robot_model/aliengo) | Collection-Aliengo-Flat, Collection-Aliengo-Rough-Blind, Collection-Aliengo-Rough-Vision |
+
+
+
+
+## Installation
+
+If you want only to deploy a trained policy on your robot, go directly [at the bottom of the readme](https://github.com/iit-DLSLab/basic-locomotion-dls-isaaclab/tree/main?tab=readme-ov-file#run-sim-to-sim-and-sim-to-real).
+
+1. Install Isaac Lab by following the [installation guide](https://github.com/isaac-sim/IsaacLab). We recommend using the conda installation as it simplifies calling Python scripts from the terminal.
+
+2. Install git for very large file
+```bash
+sudo apt install git-lfs
+```
+
+3. Clone the repository separately from the Isaac Lab installation (i.e. outside the `IsaacLab` directory)
+
+
+4. Using a python interpreter that has Isaac Lab installed, install the library
+
+```bash
+python -m pip install -e source/trash_collection_isaaclab
+```
+
+5. If you want to play with [Morphologycal Symmetries](https://arxiv.org/pdf/2403.17320), install the repo [morphosymm-rl](https://github.com/iit-DLSLab/morphosymm-rl)
+
+
+### Run a train/play in IsaacLab
+
+- To train:
+
+```bash
+python scripts/rsl_rl/train.py --task=Locomotion-Aliengo-Flat --num_envs=4096 --headless
+python scripts/rsl_rl/train.py --task=Locomotion-Aliengo-Rough-Blind --num_envs=4096 --headless
+```
+
+- To train with Symmetries, modify the related [rsl_rl_ppo_cfg.py](https://github.com/iit-DLSLab/basic-locomotion-dls-isaaclab/blob/devel/source/basic_locomotion_dls_isaaclab/basic_locomotion_dls_isaaclab/tasks/locomotion/agents/rsl_rl_ppo_cfg.py) setting *class_name = PPOSymmDataAugmented*
+```bash
+python scripts/rsl_rl/train_symm.py --task=Locomotion-Aliengo-Flat --num_envs=4096 --headless
+python scripts/rsl_rl/train_symm.py --task=Locomotion-Aliengo-Rough-Blind --num_envs=4096 --headless
+```
+
+
+- To test the policy, you can press:
+```bash
+python scripts/rsl_rl/play.py --task=Locomotion-Aliengo-Flat --num_envs=16
+python scripts/rsl_rl/play.py --task=Locomotion-Aliengo-Rough-Blind --num_envs=16
+```
+
+
+- If you have speed problem in training, may be due to cylinder collision. Then add
+
+```bash
+--kit_args="--/physics/collisionApproximateCylinders=true"
+```
+
+### Run Hyperparameter Search
+
+```bash
+echo "import ray; ray.init(); import time; [time.sleep(10) for _ in iter(int, 1)]" | python3 (TERMINAL 1)
+```
+
+```bash
+python3 ../basic_locomotion_dls_isaaclab/exts/basic_locomotion_dls_isaaclab/basic_locomotion_dls_isaaclab/hyperparameter_tuning/tuner.py --run_mode local --cfg_file ../basic_locomotion_dls_isaaclab/exts/basic_locomotion_dls_isaaclab/basic_locomotion_dls_isaaclab/hyperparameter_tuning/locomotion_aliengo_cfg.py --cfg_class LocomotionAliengoFlatTuner (TERMINAL 2)
+```
+
+
+### Convert XML to USD
+We use model from [gym-quadruped](https://github.com/iit-DLSLab/gym-quadruped).
+
+```bash
+./isaaclab.sh -p scripts/tools/convert_mjcf.py   ../basic_locomotion_dls_isaaclab/scripts/sim_to_sim_mujoco/gym-quadruped/gym_quadruped/robot_model/aliengo/aliengo.xml   ../aliengo.usd   --import-sites   --make-instanceable
+```
+
+Remember to set in the application above, "set as default prim" to the root of the robot. Furthermore, for now, add the following lines in the xml of your robots to make the feet seen as body
+
+```bash
+<body name="FL_foot" pos="0 0 -0.25">
+    <!-- FL_foot only collision -->
+    <geom name="FL" class="collision" size="0.0265" pos="0 0 0" />
+</body>
+```
+
+
+### Run Sim-to-Sim and Sim-to-Real
+
+1. install [miniforge](https://github.com/conda-forge/miniforge/releases) (x86_64 or arm64 depending on your platform)
+
+2. create an environment using the file in the folder [installation](https://github.com/iit-DLSLab/basic-locomotion-dls-isaaclab/tree/main/installation):
+
+
+```bash
+conda env create -f mamba_environment_ros1.yaml
+conda activate basic_locomotion_dls_isaaclab_ros1_env
+
+conda env create -f mamba_environment_ros2.yaml
+conda activate basic_locomotion_dls_isaaclab_ros2_env
+```
+
+3. Then you can 
+
+```bash
+## Sim-to-Sim
+python3 deploy/play_mujoco.py
+
+## Sim-to-Real with ROS1
+cd deploy/ros1_ws
+catkin_make install -j4
+python3 deploy/play_ros1.py
+
+## Sim-to-Real with ROS2
+cd deploy/ros2_ws
+colcon build
+python3 deploy/play_ros2.py 
+ros2 launch teleop_twist_joy teleop-launch.py joy_config:='xbox' (if want joystick)
+```
---
+++ source/trash_collection_isaaclab/trash_collection_isaaclab/tasks/custom_events.py
@@ -0,0 +1,179 @@
+# Copyright (c) 2022-2024, The Berkeley Humanoid Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from __future__ import annotations
+
+import torch
+from typing import TYPE_CHECKING, Literal
+
+from isaaclab.assets import Articulation
+from isaaclab.managers import SceneEntityCfg
+from isaaclab.envs.mdp.events import _randomize_prop_by_op
+
+if TYPE_CHECKING:
+    from isaaclab.envs import ManagerBasedEnv
+
+
+def randomize_joint_default_pos(
+        env: ManagerBasedEnv,
+        env_ids: torch.Tensor | None,
+        asset_cfg: SceneEntityCfg,
+        pos_distribution_params: tuple[float, float] | None = None,
+        operation: Literal["add", "scale", "abs"] = "abs",
+        distribution: Literal["uniform", "log_uniform", "gaussian"] = "uniform",
+):
+    """
+    Randomize the joint default positions which may be different from URDF due to calibration errors.
+    """
+    # extract the used quantities (to enable type-hinting)
+    asset: Articulation = env.scene[asset_cfg.name]
+
+    # resolve environment ids
+    if env_ids is None:
+        env_ids = torch.arange(env.scene.num_envs, device=asset.device)
+
+    # resolve joint indices
+    if asset_cfg.joint_ids == slice(None):
+        joint_ids = slice(None)  # for optimization purposes
+    else:
+        joint_ids = torch.tensor(asset_cfg.joint_ids, dtype=torch.int, device=asset.device)
+
+    if pos_distribution_params is not None:
+        pos = asset.data.default_joint_pos.to(asset.device).clone()
+        pos = _randomize_prop_by_op(
+            pos, pos_distribution_params, env_ids, joint_ids, operation=operation, distribution=distribution
+        )[env_ids][:, joint_ids]
+
+        if env_ids != slice(None) and joint_ids != slice(None):
+            env_ids = env_ids[:, None]
+        asset.data.default_joint_pos[env_ids, joint_ids] = pos
+
+
+
+def randomize_joint_friction_model(
+    env: ManagerBasedEnv,
+    env_ids: torch.Tensor | None,
+    asset_cfg: SceneEntityCfg,
+    friction_distribution_params: tuple[float, float] | None = None,
+    armature_distribution_params: tuple[float, float] | None = None,
+    first_order_delay_filter_distribution_params: tuple[float, float] | None = None,
+    second_order_delay_filter_distribution_params: tuple[float, float] | None = None,
+    operation: Literal["add", "scale", "abs"] = "abs",
+    distribution: Literal["uniform", "log_uniform", "gaussian"] = "uniform",
+):
+    """
+    Randomize the friction parameters used in joint friction model. 
+    """
+    # extract the used quantities (to enable type-hinting)
+    asset: Articulation = env.scene[asset_cfg.name]
+
+    # resolve environment ids
+    if env_ids is None:
+        env_ids = torch.arange(env.scene.num_envs, device=asset.device)
+
+    # resolve joint indices
+    if asset_cfg.joint_ids == slice(None):
+        joint_ids = slice(None)  # for optimization purposes
+    else:
+        joint_ids = torch.tensor(asset_cfg.joint_ids, dtype=torch.int, device=asset.device)
+
+    # sample joint properties from the given ranges and set into the physics simulation
+    # -- friction
+    if friction_distribution_params is not None:
+        for actuator in asset.actuators.values():
+            actuator_joint_ids = [joint_id in joint_ids for joint_id in actuator.joint_indices]
+            if sum(actuator_joint_ids) > 0:
+                friction = actuator.friction_static.to(asset.device).clone()
+                friction = _randomize_prop_by_op(
+                    friction, friction_distribution_params, env_ids, torch.arange(friction.shape[1]), operation=operation, distribution=distribution
+                )[env_ids][:, actuator_joint_ids]
+                actuator.friction_static[env_ids[:, None], actuator_joint_ids] = friction
+
+                friction = actuator.friction_dynamic.to(asset.device).clone()
+                friction = _randomize_prop_by_op(
+                    friction, friction_distribution_params, env_ids, torch.arange(friction.shape[1]), operation=operation, distribution=distribution
+                )[env_ids][:, actuator_joint_ids]
+                actuator.friction_dynamic[env_ids[:, None], actuator_joint_ids] = friction
+
+    if armature_distribution_params is not None:
+        for actuator in asset.actuators.values():
+            actuator_joint_ids = [joint_id in joint_ids for joint_id in actuator.joint_indices]
+            if sum(actuator_joint_ids) > 0:
+                armature = actuator.armature.to(asset.device).clone()
+                armature = _randomize_prop_by_op(
+                    armature, armature_distribution_params, env_ids, torch.arange(armature.shape[1]), operation=operation, distribution=distribution
+                )[env_ids][:, actuator_joint_ids]
+                actuator.armature[env_ids[:, None], actuator_joint_ids] = armature
+
+
+def randomize_joint_delay_model(
+    env: ManagerBasedEnv,
+    env_ids: torch.Tensor | None,
+    asset_cfg: SceneEntityCfg,
+    friction_distribution_params: tuple[float, float] | None = None,
+    armature_distribution_params: tuple[float, float] | None = None,
+    first_order_delay_filter_distribution_params: tuple[float, float] | None = None,
+    second_order_delay_filter_distribution_params: tuple[float, float] | None = None,
+    operation: Literal["add", "scale", "abs"] = "abs",
+    distribution: Literal["uniform", "log_uniform", "gaussian"] = "uniform",
+):
+
+    """
+    Randomize the delay used in joint hydraulic model. 
+    """
+    # extract the used quantities (to enable type-hinting)
+    asset: Articulation = env.scene[asset_cfg.name]
+
+    # resolve environment ids
+    if env_ids is None:
+        env_ids = torch.arange(env.scene.num_envs, device=asset.device)
+
+    # resolve joint indices
+    if asset_cfg.joint_ids == slice(None):
+        joint_ids = slice(None)  # for optimization purposes
+    else:
+        joint_ids = torch.tensor(asset_cfg.joint_ids, dtype=torch.int, device=asset.device)
+
+    if first_order_delay_filter_distribution_params is not None:
+        for actuator in asset.actuators.values():
+            actuator_joint_ids = [joint_id in joint_ids for joint_id in actuator.joint_indices]
+            if sum(actuator_joint_ids) > 0:
+                first_order_delay_filter = actuator.first_order_delay_filter.to(asset.device).clone()
+                first_order_delay_filter = _randomize_prop_by_op(
+                    first_order_delay_filter, first_order_delay_filter_distribution_params, env_ids, torch.arange(first_order_delay_filter.shape[1]), operation=operation, distribution=distribution
+                )[env_ids][:, actuator_joint_ids]
+                actuator.first_order_delay_filter[env_ids[:, None], actuator_joint_ids] = first_order_delay_filter
+
+    if second_order_delay_filter_distribution_params is not None:
+        for actuator in asset.actuators.values():
+            actuator_joint_ids = [joint_id in joint_ids for joint_id in actuator.joint_indices]
+            if sum(actuator_joint_ids) > 0:
+                second_order_delay_filter = actuator.second_order_delay_filter.to(asset.device).clone()
+                second_order_delay_filter = _randomize_prop_by_op(
+                    second_order_delay_filter, second_order_delay_filter_distribution_params, env_ids, torch.arange(second_order_delay_filter.shape[1]), operation=operation, distribution=distribution
+                )[env_ids][:, actuator_joint_ids]
+                actuator.second_order_delay_filter[env_ids[:, None], actuator_joint_ids] = second_order_delay_filter
+
+
+def zero_command_velocity(
+    env: ManagerBasedEnv,
+    env_ids: torch.Tensor,
+):
+   
+    env._commands[env_ids, 0] = 0.0
+    env._commands[env_ids, 1] = 0.0
+    env._commands[env_ids, 2] = 0.0
+
+
+def resample_command_velocity(
+    env: ManagerBasedEnv,
+    env_ids: torch.Tensor,
+):
+   
+    # Sample new commands
+    env._commands[env_ids] = torch.zeros_like(env._commands[env_ids]).uniform_(-1.0, 1.0)
+    env._commands[env_ids, 0] *= 0.5 
+    env._commands[env_ids, 1] *= 0.25 
+    env._commands[env_ids, 2] *= 0.3 
