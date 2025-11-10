667a88eb735f0ebc2d2219bd662c0c100f7db3cb
gturrisi <giulio.turrisi@iit.it>
2025-11-10 18:29:35 +0100

    added ros2 simulator

---
+++ deploy/run_simulation_and_controller_sequential.py
@@ -0,0 +1,213 @@
+# Description: This script is used to simulate the full model of the robot in mujoco
+
+# Authors:
+# Giulio Turrisi
+
+import time
+import numpy as np
+from tqdm import tqdm
+import sys
+import os 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(dir_path+"/mujoco/")
+sys.path.append(dir_path+"/../")
+sys.path.append(dir_path+"/../scripts/rsl_rl")
+
+# Gym and Simulation related imports
+import mujoco
+import mujoco.viewer
+import mujoco_utils
+from heightmap import HeightMap
+
+
+# Trash Policy imports
+from manipulation_policy_wrapper import ManipulationPolicyWrapper
+from locomotion_policy_wrapper import LocomotionPolicyWrapper
+from state_machine import StateMachine
+from state_machine import ArmStateType, GripperStateType
+
+
+import config
+import threading
+
+
+class PlayMujoco:
+    def __init__(self):
+        np.set_printoptions(precision=3, suppress=True)
+
+        self.simulation_dt = 0.002
+
+
+        # Load the model and data.
+        self.mjModel = mujoco.MjModel.from_xml_path("mujoco/models/scene_rough.xml")
+        self.mjData = mujoco.MjData(self.mjModel)
+        keyframe_id = mujoco.mj_name2id(self.mjModel, mujoco.mjtObj.mjOBJ_KEY, "home")
+        self.mjData.qpos = self.mjModel.key_qpos[keyframe_id]
+        self.viewer = mujoco.viewer.launch_passive(
+                        self.mjModel,
+                        self.mjData,
+                        show_left_ui=False,
+                        show_right_ui=False,
+                        #key_callback=lambda x: self._key_callback(x),
+                    )
+        self.last_render_time = time.time()
+        self.RENDER_FREQ = 30.0  # Hz 
+
+
+        # Initialization of variables used in the main control loop --------------------------------
+        self.manipulation_policy = ManipulationPolicyWrapper(mjModel=self.mjModel)
+        self.locomotion_policy = LocomotionPolicyWrapper(mjModel=self.mjModel)
+        self.state_machine = StateMachine()
+
+        if(self.locomotion_policy.use_vision):
+            resolution_heightmap = config.resolution_heightmap
+            num_rows_heightmap = round(config.size_x_heightmap/resolution_heightmap) + 1
+            num_cols_heightmap = round(config.size_y_heightmap/resolution_heightmap) + 1
+            self.heightmap = HeightMap(num_rows=num_rows_heightmap, num_cols=num_cols_heightmap, dist_x=resolution_heightmap, dist_y=resolution_heightmap, mj_model=mjModel, mj_data=mjData) 
+
+        self.arm_joints_position = np.zeros(6)  # 6 arm joints 
+        self.arm_joints_velocity = np.zeros(6)  # 6 arm joints
+        self.legs_joints_position = np.zeros(12)  # 12 leg joints
+        self.legs_joints_velocity = np.zeros(12)  # 12 leg joints 
+        self.desired_joint_pos_arm = self.mjData.qpos[19:25] 
+        self.desired_joint_pos_leg = self.mjData.qpos[7:19]
+        self.desired_pose_command = np.zeros(2)
+        self.desired_pose_command_overwrite = np.zeros(2)
+        self.Kp_legs = 0
+        self.Kd_legs = 0
+        self.Kp_arm = 0
+        self.Kd_arm = 0
+
+        # --------------------------------------------------------------
+        self.ref_base_lin_vel_H = np.array([0.0, 0.0, 0.0])  # Desired base linear velocity in the horizontal plane (x, y, z)
+        self.ref_base_ang_yaw_dot = 0.0  # Desired base angular velocity around the vertical axis
+
+        # Interactive Command Line
+        from console import Console
+        self.console = Console(controller_node=self)
+        thread_console = threading.Thread(target=self.console.interactive_command_line)
+        thread_console.daemon = True
+        thread_console.start()
+
+        self.console.isDown = False  # Only in this play_mujoco script
+        self.console.isRLActivated = True  # Only in this play_mujoco script
+
+
+    def run(self):
+        step_num = 1
+        # Run the simulation
+        while self.viewer.is_running():
+            step_start = time.time()
+            
+            # Get the current state of the robot -----------------------------------------------------
+            qpos, qvel = self.mjData.qpos, self.mjData.qvel
+            base_lin_vel = mujoco_utils.base_lin_vel(self.mjData, frame='base')
+            base_ang_vel = mujoco_utils.base_ang_vel(self.mjData, frame='base')
+            base_ori_euler_xyz = mujoco_utils.base_ori_euler_xyz(self.mjData)
+            heading_orientation_SO3 = mujoco_utils.heading_orientation_SO3(self.mjData)
+            base_quat_wxyz = qpos[3:7]
+            base_pos = mujoco_utils.base_pos(self.mjData)
+            self.arm_joints_position = qpos[19:25]
+
+            joints_pos_leg = qpos[7:19]
+            joints_pos_arm = qpos[19:25]
+            joints_pos_gripper = qpos[25]
+
+            joints_vel_leg = qvel[6:18]
+            joints_vel_arm = qvel[18:24]
+            joints_vel_gripper = qvel[24]
+
+        
+            ref_base_lin_vel, ref_base_ang_vel = mujoco_utils.target_base_vel(self.mjData, self.ref_base_lin_vel_H, self.ref_base_ang_yaw_dot, frame='world')
+
+
+            if(self.locomotion_policy.use_vision):
+                self.heightmap.update_height_map(self.mjData.qpos[0:3], yaw=base_ori_euler_xyz[2])
+
+
+            if(self.console.isDown):
+                # Impedence Loop
+                self.Kp_legs = self.locomotion_policy.Kp_stand_up_and_down
+                self.Kd_legs = self.locomotion_policy.Kd_stand_up_and_down
+
+            # RL controller --------------------------------------------------------------
+            if self.console.isRLActivated and step_num % round(1 / (self.locomotion_policy.RL_FREQ * self.simulation_dt)) == 0:            
+                
+                if self.state_machine.state_type == ArmStateType.REACH:
+                    self.desired_joint_pos_arm, self.desired_pose_command = self.manipulation_policy.compute_control(
+                                base_pos=base_pos, 
+                                base_ori_euler_xyz=base_ori_euler_xyz, 
+                                base_quat_wxyz=base_quat_wxyz,
+                                base_lin_vel=base_lin_vel, 
+                                base_ang_vel=base_ang_vel,
+                                heading_orientation_SO3=heading_orientation_SO3,
+                                joints_pos_leg=joints_pos_leg, 
+                                joints_vel_leg=joints_vel_leg,
+                                joints_pos_arm=joints_pos_arm,
+                                joints_vel_arm=joints_vel_arm,
+                                ref_base_lin_vel=ref_base_lin_vel, 
+                                ref_base_ang_vel=ref_base_ang_vel,
+                                heightmap_data=self.heightmap.data if self.locomotion_policy.use_vision else None)
+                else:
+                    self.desired_joint_pos_arm = self.state_machine.desired_position
+                    self.desired_pose_command = np.zeros(2)
+
+                self.Kp_arm = self.manipulation_policy.Kp_arm
+                self.Kd_arm = self.manipulation_policy.Kd_arm
+
+
+                self.desired_joint_pos_leg = self.locomotion_policy.compute_control(
+                            base_pos=base_pos, 
+                            base_ori_euler_xyz=base_ori_euler_xyz, 
+                            base_quat_wxyz=base_quat_wxyz,
+                            base_lin_vel=base_lin_vel, 
+                            base_ang_vel=base_ang_vel,
+                            heading_orientation_SO3=heading_orientation_SO3,
+                            joints_pos_leg=joints_pos_leg, 
+                            joints_vel_leg=joints_vel_leg,
+                            joints_pos_arm=joints_pos_arm,
+                            ref_base_lin_vel=ref_base_lin_vel, 
+                            ref_base_ang_vel=ref_base_ang_vel,
+                            ref_pose_command=self.desired_pose_command + self.desired_pose_command_overwrite,
+                            heightmap_data=self.heightmap.data if self.locomotion_policy.use_vision else None)
+
+                self.Kp_legs = self.locomotion_policy.Kp_walking
+                self.Kd_legs = self.locomotion_policy.Kd_walking
+
+            
+            # PD controller --------------------------------------------------------------
+            error_joints_pos_leg = self.desired_joint_pos_leg - joints_pos_leg
+            tau_leg = self.Kp_legs*error_joints_pos_leg - self.Kd_legs*joints_vel_leg
+
+            error_joints_pos_arm = self.desired_joint_pos_arm - joints_pos_arm
+            tau_arm = self.Kp_arm*error_joints_pos_arm - self.Kd_arm*joints_vel_arm
+
+            error_gripper_pos = self.state_machine.desired_gripper_position - joints_pos_gripper
+            tau_gripper = config.Kp_gripper*error_gripper_pos - config.Kd_gripper*joints_vel_gripper
+
+            
+            # Set control and mujoco step ----------------------------------------------------------------------
+            self.mjData.ctrl[0:12] = tau_leg
+            self.mjData.ctrl[12:18] = tau_arm
+            self.mjData.ctrl[18] = tau_gripper
+            mujoco.mj_step(self.mjModel, self.mjData)
+            step_num = step_num +1
+
+
+            # Sleep to match real-time ---------------------------------------------------------
+            loop_elapsed_time = time.time() - step_start
+            if(loop_elapsed_time < self.simulation_dt):
+                time.sleep(self.simulation_dt - (loop_elapsed_time))
+
+
+            # Render only at a certain frequency -----------------------------------------------------------------
+            if time.time() - self.last_render_time > 1.0 / self.RENDER_FREQ:
+                # Update the camera position
+                self.viewer.cam.lookat[:] = base_pos
+                self.viewer.sync()
+                self.last_render_time = time.time()
+
+
+if __name__ == "__main__":
+    play_mujoco = PlayMujoco()
+    play_mujoco.run()
---
+++ deploy/run_simulation_ros2.py
@@ -0,0 +1,184 @@
+# Description: This script is used to run a mujoco simulation in ROS2
+
+# Authors:
+# Giulio Turrisi
+
+import os
+import subprocess
+
+def source_ros_install(install_path):
+    # Get environment variables from a sourced shell
+    command = ['bash', '-c', f'source {install_path}/setup.bash && env']
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE, text=True)
+    for line in proc.stdout:
+        key, _, value = line.partition("=")
+        os.environ[key] = value.strip()
+    proc.communicate()
+
+# Build and source the ROS2 workspace if not already built:
+ros2_ws_path = os.path.dirname(os.path.realpath(__file__)) + "/ros2_ws"
+if not os.path.exists(ros2_ws_path+"/install"):
+    print("Building the ROS2 workspace...")
+    os.system(" \
+    cd ros2_ws && \
+    colcon build")
+source_ros_install("ros2_ws/install")
+
+
+import rclpy 
+from rclpy.node import Node 
+from dls2_interfaces.msg import BaseStateMsg, BlindStateMsg, ArmBlindState, TrajectoryGeneratorMsg
+
+import time
+import numpy as np
+import sys
+import os 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(dir_path+"/mujoco/")
+sys.path.append(dir_path+"/../")
+
+# Gym and Simulation related imports
+import mujoco
+import mujoco.viewer
+import mujoco_utils
+from heightmap import HeightMap
+
+
+class MujocoSimulationNode(Node):
+    def __init__(self):
+        super().__init__('Mujoco_Simulation_Node')
+
+        self.simulation_dt = 0.002
+
+        # Load the model and data.
+        self.mjModel = mujoco.MjModel.from_xml_path("mujoco/models/scene_rough.xml")
+        self.mjData = mujoco.MjData(self.mjModel)
+        keyframe_id = mujoco.mj_name2id(self.mjModel, mujoco.mjtObj.mjOBJ_KEY, "down")
+        self.mjData.qpos = self.mjModel.key_qpos[keyframe_id]
+        self.viewer = mujoco.viewer.launch_passive(
+                        self.mjModel,
+                        self.mjData,
+                        show_left_ui=False,
+                        show_right_ui=False,
+                        #key_callback=lambda x: self._key_callback(x),
+                    )
+        self.last_render_time = time.time()
+        self.RENDER_FREQ = 30.0  # Hz 
+
+        # Subscribers and Publishers
+        self.publisher_base_state = self.create_publisher(BaseStateMsg,"/dls2/base_state", 1)
+        self.publisher_blind_state = self.create_publisher(BlindStateMsg,"/dls2/blind_state", 1)
+        self.publisher_arm_blind_state = self.create_publisher(ArmBlindState,"/dls2/arm_blind_state", 1)
+        self.subscriber_trajectory_generator_arm = self.create_subscription(TrajectoryGeneratorMsg,"dls2/arm_trajectory_generator", self.get_arm_trajectory_generator_callback, 1)
+        self.subscriber_trajectory_generator_legs = self.create_subscription(TrajectoryGeneratorMsg,"dls2/trajectory_generator", self.get_legs_trajectory_generator_callback, 1)
+        
+        self.timer = self.create_timer(self.simulation_dt, self.compute_simulator_step_callback)
+
+        # Desired PD
+        qpos, qvel = self.mjData.qpos, self.mjData.qvel
+        self.desired_arm_joints_position = qpos[19:25]
+        self.desired_legs_joints_position = qpos[7:19]
+        self.desired_gripper_position = qpos[25]
+        self.Kp_legs = 0
+        self.Kd_legs = 0
+        self.Kp_arm = 0
+        self.Kd_arm = 0
+        self.Kp_gripper = 0.0
+        self.Kd_gripper = 0.0
+
+
+    def get_arm_trajectory_generator_callback(self, msg):
+
+        joints_position = np.array(msg.joints_position)
+
+        self.desired_arm_joints_position = joints_position
+        self.Kp_arm = np.array(msg.kp_gains)
+        self.Kd_arm = np.array(msg.kd_gains)
+
+    def get_legs_trajectory_generator_callback(self, msg):
+        
+        # Desired leg joints position
+        joints_position = np.array(msg.joints_position)
+        self.desired_legs_joints_position = joints_position
+        self.Kp_legs = np.array(msg.kp_gains)
+        self.Kd_legs = np.array(msg.kd_gains)
+
+
+    def compute_simulator_step_callback(self):
+        
+
+        # Get the current state of the robot -----------------------------------------------------
+        qpos, qvel = self.mjData.qpos, self.mjData.qvel
+        base_lin_vel = mujoco_utils.base_lin_vel(self.mjData, frame='base')
+        base_ang_vel = mujoco_utils.base_ang_vel(self.mjData, frame='base')
+        base_pos = mujoco_utils.base_pos(self.mjData)
+        self.arm_joints_position = qpos[19:25]
+
+        joints_pos_leg = qpos[7:19]
+        joints_pos_arm = qpos[19:25]
+        joints_pos_gripper = qpos[25]
+
+        joints_vel_leg = qvel[6:18]
+        joints_vel_arm = qvel[18:24]
+        joints_vel_gripper = qvel[24]
+
+        error_joints_pos_leg = self.desired_legs_joints_position - joints_pos_leg
+        tau_leg = self.Kp_legs*error_joints_pos_leg - self.Kd_legs*joints_vel_leg
+
+        error_joints_pos_arm = self.desired_arm_joints_position - joints_pos_arm
+        tau_arm = self.Kp_arm*error_joints_pos_arm - self.Kd_arm*joints_vel_arm
+
+        error_gripper_pos = self.desired_gripper_position - joints_pos_gripper
+        tau_gripper = self.Kp_gripper*error_gripper_pos - self.Kd_gripper*joints_vel_gripper
+
+
+        # Apply the torques and step the simulation ------------------------------------------------
+        self.mjData.ctrl[0:12] = tau_leg
+        self.mjData.ctrl[12:18] = tau_arm
+        self.mjData.ctrl[18] = tau_gripper
+        mujoco.mj_step(self.mjModel, self.mjData)
+
+
+        # Publish the state of the robot ----------------------------------------------------------
+        base_state_msg = BaseStateMsg()
+        base_state_msg.position = base_pos
+        base_state_msg.orientation = np.roll(self.mjData.qpos[3:7],-1)
+        base_state_msg.linear_velocity = base_lin_vel
+        base_state_msg.angular_velocity = base_ang_vel
+        self.publisher_base_state.publish(base_state_msg)
+
+        blind_state_msg = BlindStateMsg()
+        blind_state_msg.joints_position = self.mjData.qpos[7:19]
+        blind_state_msg.joints_velocity = self.mjData.qvel[6:18]
+        self.publisher_blind_state.publish(blind_state_msg)
+
+        arm_blind_state_msg = ArmBlindState()
+        arm_blind_state_msg.joints_position = self.mjData.qpos[19:25]
+        arm_blind_state_msg.joints_velocity = self.mjData.qvel[18:24]
+        self.publisher_arm_blind_state.publish(arm_blind_state_msg)
+
+
+        # Render only at a certain frequency -----------------------------------------------------------------
+        if time.time() - self.last_render_time > 1.0 / self.RENDER_FREQ:
+            # Update the camera position
+            self.viewer.cam.lookat[:] = base_pos
+            self.viewer.sync()
+            self.last_render_time = time.time()
+
+
+
+
+#---------------------------
+if __name__ == '__main__':
+    
+    print('Hello from the mujoco simulation node.')
+    
+    rclpy.init()
+    mujoco_simulation_node = MujocoSimulationNode()
+    rclpy.spin(mujoco_simulation_node)
+
+    mujoco_simulation_node.destroy_node()
+    rclpy.shutdown()
+
+    print("mujoco simulation node is stopped")
+    exit(0)
