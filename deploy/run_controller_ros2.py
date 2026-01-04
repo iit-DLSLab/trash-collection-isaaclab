fix detection in sim - rotation still to be fixed

deploy/run_controller_ros2.py
@@ -132,20 +132,6 @@ class TrashControlNode(Node):
 
         # Variables for IK from detection
         self.received_detection = False
-        self.ik_goal_orient_camera_frame = np.array([1.0, 0.0 ,0.0 ,0.0])
-        self.r_optical_to_camera_frame = np.array([
-                                                [0.0, 0.0,  1.0],
-                                                [-1.0, 0.0, 0.0],
-                                                [0.0, -1.0, 0.0]
-                                            ])
-        self.r_camera_to_base_frame = np.array([
-                                                [1.0, 0.0,  0.0],
-                                                [0.0, 1.0, 0.0],
-                                                [0.0, 0.0, 1.0]
-                                            ])
-
-        self.r_camera_frame_to_base = self.r_camera_to_base_frame @ self.r_optical_to_camera_frame
-
         self.ik_goal_camera_frame = np.zeros(3)
         self.ik_goal_base_frame = np.zeros(3)
         self.ik_goal_orient_base_frame = np.array([1.0, 0.0 ,0.0 ,0.0])
@@ -164,21 +150,62 @@ class TrashControlNode(Node):
 
 
     def get_grasp_pose_callback(self, msg):
+        # Trasformation from camera frame to base frame
+        body_id = mujoco.mj_name2id(self.mjModel, mujoco.mjtObj.mjOBJ_BODY, "trunk")
+        cam_id  = mujoco.mj_name2id(self.mjModel, mujoco.mjtObj.mjOBJ_CAMERA, "robotcam")
+
+        p_WB = self.mjData.xpos[body_id]
+        R_WB = self.mjData.xmat[body_id].reshape(3, 3)
+
+        p_WC = self.mjData.cam_xpos[cam_id]
+        R_WC = self.mjData.cam_xmat[cam_id].reshape(3, 3)
+
+        R_BC = R_WB.T @ R_WC
+        t_BC = R_WB.T @ (p_WC - p_WB)
+
+        self.r_optical_to_camera_frame = np.array([
+                                                [0.0, 0.0,  1.0],
+                                                [-1.0, 0.0, 0.0],
+                                                [0.0, -1.0, 0.0]
+                                            ])
+
         if len(msg.poses) > 0:
             pose = msg.poses[0]
             self.ik_goal_camera_frame[0] = pose.position.x
             self.ik_goal_camera_frame[1] = pose.position.y
             self.ik_goal_camera_frame[2] = pose.position.z
 
-            #conversion from camera optical frame to base
-            self.ik_goal_base_frame =  self.r_camera_frame_to_base @ self.ik_goal_camera_frame
-
             self.ik_goal_orient_camera_frame[0] = pose.orientation.w
             self.ik_goal_orient_camera_frame[1] = pose.orientation.x
             self.ik_goal_orient_camera_frame[2] = pose.orientation.y
             self.ik_goal_orient_camera_frame[3] = pose.orientation.z
 
-            self.ik_goal_orient_base_frame = ( R.from_matrix(self.r_camera_frame_to_base) * R.from_quat(self.ik_goal_orient_camera_frame, scalar_first=True)).as_quat(scalar_first=True)
+            #conversion from camera optical frame to base
+            #self.ik_goal_base_frame =  self.r_camera_frame_to_base @ self.ik_goal_camera_frame
+            #self.ik_goal_orient_base_frame = ( R.from_matrix(self.r_camera_frame_to_base) * R.from_quat(self.ik_goal_orient_camera_frame, scalar_first=True)).as_quat(scalar_first=True)
+            
+            # --- position ---
+            p_C = self.ik_goal_camera_frame
+            p_B = R_BC @ p_C + t_BC
+
+            # --- orientation ---
+            # Convert q_C to rotation matrix
+            R_C = np.zeros((3, 3), dtype=float)
+            mujoco.mju_quat2Mat(R_C.reshape(9,), self.ik_goal_orient_camera_frame)
+            R_C = R_C.reshape(3,3)
+
+            # Compose: body_R = (body<-camera) * (camera_R)
+            R_B = R_BC @ R_C
+
+            # Convert back to quaternion (w,x,y,z)
+            q_B = np.zeros(4, dtype=float)
+            mujoco.mju_mat2Quat(q_B, R_B.reshape(9,))
+            R_B = R_B.reshape(3,3)
+
+            self.ik_goal_base_frame = p_B
+            self.ik_goal_orient_base_frame = q_B
+
+
             self.received_detection = True
 
     

deploy/run_simulator_ros2.py
@@ -154,23 +154,30 @@ class MujocoSimulationNode(Node):
         #TODO: to publish in camera frame
         detections_msg = PoseArray()
         detections_msg.header.stamp = self.get_clock().now().to_msg()
-        # For each bottle in the scene
-        for bottle_id in range(2):
-            bottle_pos = self.mjData.xpos[self.mjModel.body('bottle'+str(bottle_id)).id]
-            bottle_quat = self.mjData.xquat[self.mjModel.body('bottle'+str(bottle_id)).id]
 
-            # The bottle is in world frame, trasform it to camera frame
-            # TODO, extract the camera pose in world frame and do the transformation
+        # --- World-frame positions ---
+        p_WB = self.mjData.xpos[self.mjModel.body('bottle').id].copy()
+        R_WB = self.mjData.xmat[self.mjModel.body('bottle').id].copy().reshape(3, 3)
 
-            detection_pose = Pose()
-            detection_pose.position.x = bottle_pos[0]
-            detection_pose.position.y = bottle_pos[1]
-            detection_pose.position.z = bottle_pos[2]
-            detection_pose.orientation.w = bottle_quat[0]
-            detection_pose.orientation.x = bottle_quat[1]
-            detection_pose.orientation.y = bottle_quat[2]
-            detection_pose.orientation.z = bottle_quat[3]
-            detections_msg.poses.append(detection_pose)
+        cam_id  = mujoco.mj_name2id(self.mjModel, mujoco.mjtObj.mjOBJ_CAMERA, "robotcam")
+        p_WC = self.mjData.cam_xpos[cam_id].copy()     # camera origin in world
+        R_WC = self.mjData.cam_xmat[cam_id].copy().reshape(3, 3)
+
+        # --- Transform world → camera ---
+        p_CB = R_WC.T @ (p_WB - p_WC)
+        R_CB = R_WC.T @ R_WB
+        q_cb = np.zeros(4)
+        mujoco.mju_mat2Quat(q_cb, R_CB.reshape(9,))
+
+        detection_pose = Pose()
+        detection_pose.position.x = p_CB[0]
+        detection_pose.position.y = p_CB[1]
+        detection_pose.position.z = p_CB[2]
+        detection_pose.orientation.w = q_cb[0]
+        detection_pose.orientation.x = q_cb[1]
+        detection_pose.orientation.y = q_cb[2]
+        detection_pose.orientation.z = q_cb[3]
+        detections_msg.poses.append(detection_pose)
         self.publisher_detections.publish(detections_msg)
 
 