6d09db0c00bd75fd77c175e7e61f17701d834288
giulioturrisi <giulioturrisi@outlook.it>
2026-01-20 20:43:33 +0100

    cleaning

--- source/trash_collection_isaaclab/trash_collection_isaaclab/tasks/locomotion/locomotion_env_cfg.py
+++ source/trash_collection_isaaclab/trash_collection_isaaclab/tasks/locomotion/locomotion_env_cfg.py
@@ -263,9 +263,9 @@ class AliengoFlatEnvCfg(DirectRLEnvCfg):
     joints_torque_reward_scale = -2.5e-6
     joints_accel_reward_scale = -2.5e-7
     joints_energy_reward_scale = -1e-4
-    joints_hip_position_reward_scale = -0.1
-    joints_thigh_position_reward_scale = -0.1
-    joints_calf_position_reward_scale = -0.001
+    joints_hip_position_reward_scale = -0.1 * 0.0
+    joints_thigh_position_reward_scale = -0.1 * 0.0
+    joints_calf_position_reward_scale = -0.001 * 0.0
    
     
     # Undesired contacts reward scale
@@ -280,8 +280,8 @@ class AliengoFlatEnvCfg(DirectRLEnvCfg):
     feet_slide_reward_scale = -0.25 * 0.0
     feet_contact_suggestion_reward_scale =  0.25
     feet_to_base_distance_reward_scale = 0.25 * 0.0
-    
     feet_to_hip_distance_reward_scale = 1.5
+    
     # This is used in loocmotion_env.py for the above reward
     desired_hip_offset = 0.083
     
