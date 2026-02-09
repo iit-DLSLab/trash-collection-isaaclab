4816b14be4a8f5decff565e116f2b4763c5e944b
giulioturrisi <giulioturrisi@hotmail.it>
2026-02-09 19:28:35 +0100

    modified sh for more automatic run (e.g. dls2 connect now source ros2_ws)

--- deploy/ros2_localhost_connect.sh
+++ deploy/ros2_localhost_connect.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Remember to run this file with: source ros2_localhost_connect.sh"
 export ROS_LOCALHOST_ONLY=1
+unset ROS_DISCOVERY_SERVER
 ros2 daemon stop &&
 ros2 daemon start
 echo "ROS2 configured to use localhost only."
