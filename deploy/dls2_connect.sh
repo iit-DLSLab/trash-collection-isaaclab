40db77dc0b046e6f6d81eaeb35e78f8631d39dc7
giulioturrisi <giulioturrisi@hotmail.it>
2026-02-09 19:36:28 +0100

    unset some variable set by automatic sh script

--- deploy/dls2_connect.sh
+++ deploy/dls2_connect.sh
@@ -2,6 +2,7 @@
 echo "Remeber to run this file with: source dls2_connect.sh"
 export ROS_DISCOVERY_SERVER=127.0.0.1:11814 && 
 export ROS_SUPER_CLIENT=TRUE && 
+export ROS_LOCALHOST_ONLY=0 &&
 ros2 daemon stop &&
 ros2 daemon start
 
--- deploy/ros2_localhost_connect.sh
+++ deploy/ros2_localhost_connect.sh
@@ -2,6 +2,7 @@
 echo "Remember to run this file with: source ros2_localhost_connect.sh"
 export ROS_LOCALHOST_ONLY=1
 unset ROS_DISCOVERY_SERVER
+unset ROS_SUPER_CLIENT
 ros2 daemon stop &&
 ros2 daemon start
 echo "ROS2 configured to use localhost only."
